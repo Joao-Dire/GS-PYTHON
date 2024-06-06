fdabc695a6625e013566cd93b40eb15e5c679d56
joao <vitordire@gmail.com>
2024-06-06 00:06:22 -0300

    finalização do projeto

--- OceanHelp.py
+++ OceanHelp.py
@@ -1,50 +1,74 @@
 import os
 
 def limpa_tela():
-    os.system('cls')  # limpa a tela para mostrar o resultado
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main_content():
-    print('ʙᴇᴍ ᴠɪɴᴅᴏ ᴀᴏ ᴏᴄᴇᴀɴʜᴇʟᴘ')
-    print('(1) Denunciar')
-    print('(2) Doar para ONGS')
-    print('(3) Lista de animais em extinção')
-    escolha = int(input('Escolha uma opção\n'))
-    while escolha < 1 or escolha > 3:
+    while True:
         print('ʙᴇᴍ ᴠɪɴᴅᴏ ᴀᴏ ᴏᴄᴇᴀɴʜᴇʟᴘ')
         print('(1) Denunciar')
-        print('(2) Doar para ONGS')
-        print('(3) Lista de animais em extinção')       
-        escolha = int(input('opção inválida! Escolha novamente\n'))
-    return escolha
+        print('(2) Doar para ONGs')
+        print('(3) Lista de animais em extinção')
+        print('(4) Sair')
+        escolha = int(input('Escolha uma opção\n'))
+        
+        while escolha < 1 or escolha > 4:
+            print('ʙᴇᴍ ᴠɪɴᴅᴏ ᴀᴏ ᴏᴄᴇᴀɴʜᴇʟᴘ')
+            print('(1) Denunciar')
+            print('(2) Doar para ONGs')
+            print('(3) Lista de animais em extinção')        
+            escolha = int(input('Opção inválida! Escolha novamente\n'))
+
+        if escolha == 1:
+            limpa_tela()
+            denuncia()
+        elif escolha == 2:
+            limpa_tela()
+            doacao()
+        elif escolha == 3:
+            limpa_tela()
+            lista_animais()
+        elif escolha == 4:
+            break
 
 def denuncia():
-
     print('O que gostaria de denunciar?')
-    print('(1) Maltratos a animais (pesca ilegal, trafico de animais, etc.')
+    print('(1) Maltratos a animais (pesca ilegal, tráfico de animais, etc.')
     print('(2) Poluição de rios, oceanos ou/e praias')
     print('(3) Animal em situação vulnerável (Resgate)')
+    print('(4) Voltar')
     escolha = int(input('Escolha uma opção\n'))
-    while escolha < 1 or escolha > 3:
-        escolha = int(input('opção inválida! Escolha novamente\n'))
+    
+    while escolha < 1 or escolha > 4:
+        escolha = int(input('Opção inválida! Escolha novamente\n'))
+
     if escolha == 1 or escolha == 2 or escolha == 3:
         caso_denuncia = input('Por favor, descreva o que você viu\n')
-        local_denucia = input('Ok! Agora me diga o local em que ocorreu\n')
+        local_denuncia = input('Ok! Agora me diga o local em que ocorreu\n')
         data_denuncia = input('Qual foi a data do ocorrido?\n')
-        confirmacao = int(input(f'Ok, antes de enviar confirme as informações.\nCaso: {caso_denuncia}\nLocal: {local_denucia}\nData: {data_denuncia}\n(1) Confirmar\n(2) Cancelar\n'))
+        confirmacao = int(input(f'Ok, antes de enviar confirme as informações.\nCaso: {caso_denuncia}\nLocal: {local_denuncia}\nData: {data_denuncia}\n(1) Confirmar\n(2) Cancelar\n'))
+        
         while confirmacao == 2:
             caso_denuncia = input('Por favor, descreva o que você viu\n')
-            local_denucia = input('Ok! Agora me diga o local em que ocorreu\n')
+            local_denuncia = input('Ok! Agora me diga o local em que ocorreu\n')
             data_denuncia = input('Qual foi a data do ocorrido?\n')
-            confirmacao = int(input(f'Ok, antes de enviar confirme as informações.\nCaso: {caso_denuncia}\nLocal: {local_denucia}\nData: {data_denuncia}\n 1 - confirmar\n2 - Cancelar\n'))
+            confirmacao = int(input(f'Ok, antes de enviar confirme as informações.\nCaso: {caso_denuncia}\nLocal: {local_denuncia}\nData: {data_denuncia}\n 1 - confirmar\n2 - Cancelar\n'))
+        
         print('Obrigado! Sua atitude salva vidas!')
+
+    elif escolha == 4:
+        limpa_tela()
+        return
 
 def doacao():
     print('Obrigado por ajudar!')
     valor = float(input('Qual seria o valor?\nR$'))
+
     while valor < 0:
         valor = float(input('Valor incorreto! Insira um novo valor\nR$'))
     
     metodo_pagamento = int(input('Qual seria o método de pagamento?\n(1) Cartão Débito\n(2) Pix\n'))
+
     if metodo_pagamento == 1:
         metodo_pagamento_str = 'Cartão de Débito'
     elif metodo_pagamento == 2:
@@ -59,8 +83,7 @@ def doacao():
     while confirmacao == 2:
         valor = float(input('Qual seria o valor?\nR$'))
         while valor < 0:
-            valor = float(input('Valor incorreto! Insira um novo valor\nR$'))
-        
+            valor = float(input('Valor incorreto! Insira um novo valor\nR$'))      
         metodo_pagamento = int(input('Qual seria o método de pagamento?\n(1) Cartão Débito\n(2) Pix\n'))
         if metodo_pagamento == 1:
             metodo_pagamento_str = 'Cartão de Débito'
@@ -69,26 +92,43 @@ def doacao():
         else:
             print("Método de pagamento inválido!")
             return
-
         limpa_tela()
         confirmacao = int(input(f'Confirme os dados: R${valor}\nMétodo de pagamento: {metodo_pagamento_str}\n(1) Confirmar\n(2) Cancelar\n'))
-    
+           
     if metodo_pagamento == 1:
         num_cartao = input("Número impresso no cartão:")
         dat_validade = input('Data de vencimento:')
         cvv = input('CVV do cartão:')
         print(f'Confirme os dados\nNúmero do cartão: {num_cartao}\nData de validade: {dat_validade}\nCVV: {cvv}')
     
-    print("Doação realizada com sucesso!")
+    print("Doação realizada com sucesso!")
 
+def adicionar_animais(lista):
+    raca = input('Digite a raça do animal:\n')
+    especie = input('Digite a espécie:\n')
+    nome = input('Digite o nome científico do animal:\n')
+    lista.append(raca)
+    lista.append(especie)
+    lista.append(nome)
 
-if __name__ == '__main__':
-    escolha = main_content()
-    if escolha == 1:
-        limpa_tela()
-        denuncia()
-    elif escolha == 2:
-        limpa_tela()
-        doacao()
-    elif escolha == 3:
-        print('Função de lista de animais em extinção ainda não implementada.')
+def lista_animais():
+    while True:
+        print('(1) Adicionar animais em extinção')
+        print('(2) Listar animais em extinção')
+        print('(3) Voltar')
+        escolha = int(input('Escolha uma opção\n'))
+        
+        while escolha < 1 or escolha > 3:
+            escolha = int(input('Opção inválida! Escolha novamente\n'))
+
+        if escolha == 1:
+            adicionar_animais(animais)
+        elif escolha == 2:
+            print(f'Animais em extinção: {animais}')
+        elif escolha == 3:
+            limpa_tela()
+            return
+
+animais = []
+
+op = main_content()
